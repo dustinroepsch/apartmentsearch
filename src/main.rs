f8f456b07c34438d4298f87de344c8060a40cf98
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2022-01-08 09:19:43 -0800

    add idea to gitignore and parse cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+enum Opt {
+    /// Search with an arbitrary string
+    Search {
+        /// The string to search with the google maps direction api
+        query: String,
+    },
+    /// Search with a lat long
+    LatLong {},
+}
 fn main() {
-    println!("Hello, world!");
+    const API_KEY: &str = include_str!("../api_key.txt");
+    let opt = Opt::from_args();
+    println!("{:?}", opt)
 }

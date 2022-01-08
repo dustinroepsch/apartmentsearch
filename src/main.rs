5f18ff794b1a8721038389393733667cc17d3fe5
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2022-01-08 09:46:48 -0800

    add valer and my work addresses to bin and make a test call to measure the distance

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-
+use google_maps::directions::{Location, TravelMode};
+use google_maps::{ClientSettings, LatLng};
+use rust_decimal_macros::dec;
+use std::error::Error;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -11,8 +14,27 @@ enum Opt {
     /// Search with a lat long
     LatLong {},
 }
-fn main() {
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     const API_KEY: &str = include_str!("../api_key.txt");
-    let opt = Opt::from_args();
-    println!("{:?}", opt)
+
+    let dustin_work_address: Location =
+        Location::LatLng(LatLng::try_from(dec!(47.6430149), dec!(-122.1402896))?);
+
+    let valery_work_address: Location =
+        Location::LatLng(LatLng::try_from(dec!(47.9300239), dec!(-122.65863))?);
+
+    let google_maps_client = ClientSettings::new(API_KEY);
+
+    let response = google_maps_client
+        .directions(dustin_work_address, valery_work_address)
+        .with_travel_mode(TravelMode::Driving)
+        .execute()
+        .await?;
+    println!("{:#?}", response);
+
+    let _opt = Opt::from_args();
+
+    Ok(())
 }
